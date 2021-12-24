8afee837dac32cc8c5ba5aba7f4db90771eadfcc
mascaradee <lamascaradee@gmail.com>
2021-12-24 18:00:52 +0900

    classic vesion 앱 만들기 - 어려움

--- src/App.js
+++ src/App.js
@@ -1,25 +1,60 @@
-import logo from './logo.svg';
-import './App.css';
+import React from "react";
+import axios from "axios";
+import Movie from "./Movie";
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+/**
+ * axios = fetch 역할
+ * async-await 특정 역할이 끝날때까지 기다려라
+ */
+class App extends React.Component {
+  state = {
+    isLoading: true,
+    movies: [],
+  };
+  /**
+   * const [isLoading, setLoading] = useState(true)
+   * const [movies, setMovies] = useState([]])
+   */
+  getMovies = async () => {
+    const {
+      data: {
+        data: { movies },
+      },
+    } = await axios.get(
+      "https://yts-proxy.now.sh/list_movies.json?sort_by=rating"
+    );
+    this.setState({ movies, isLoading: false }); //  movies: movies => 이름과 값이 같으면 하나만 쓴다
+  };
+  /**
+   * fetch를 이용했고
+   */
+  componentDidMount() {
+    this.getMovies();
+  }
+  render() {
+    const { isLoading, movies } = this.state; // ES6  문법 / state 변수명 스펠링 유의할것
+    return (
+      <div>
+        {isLoading
+          ? "Loading"
+          : movies.map((movie) => {
+              return (
+                <Movie
+                  key={movie.id}
+                  id={movie.id}
+                  year={movie.year}
+                  title={movie.title}
+                  summary={movie.summary}
+                  poster={movie.medium_cover_image}
+                />
+              );
+            })}
+      </div>
+    );
+  }
+  /**
+   * render() 함수도 계속 호출하지 않았음
+   */
 }
 
 export default App;
